2885ea8adf895e4c1d951d341dc9b1a511ef8b45
Pradheon <pradheon@gmail.com>
2022-02-01 14:19:54 -0800

    Initial Commit

---
+++ JoshanRai-UnitConverter/JoshanRai_UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoshanRai_UnitConverterApp.swift
+//  JoshanRai-UnitConverter
+//
+//  Created by Joshan Rai on 2/1/22.
+//
+
+import SwiftUI
+
+@main
+struct JoshanRai_UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
